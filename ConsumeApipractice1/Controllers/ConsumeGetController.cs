37c5bf9930af1c42f095875c468bcb5825c036e1
Jesuraj77 <isysraju2001@gmail.com>
2020-03-22 14:10:08 +0530

    Code added

---
+++ ConsumeApipractice1/Controllers/ConsumeGetController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ConsumeApipractice1.Controllers
+{
+    public class ConsumeGetController : Controller
+    {
+       // code here to fetch the resources via HHTP client from API and MVC client
+    }
+}
